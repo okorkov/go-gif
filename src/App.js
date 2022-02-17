7bbb25743759629ba3f2797d6e4d07629b5d8a24
okorkov <alex.okorkov@gmail.com>
2022-02-17 00:31:47 -0800

    initial app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { Button } from "./components/Button";
+import { Inputfile } from "./components/Inputfile";
+import { Header } from "./components/Header";
+import { Resultimg } from "./components/Resultimg";
+import { Inputvideo } from "./components/Inputvideo";
+import { Dbutton } from "./components/Dbutton";
+
+// Create the FFmpeg instance and load it
+const ffmpeg = createFFmpeg({ log: true, corePath: "/ffmpeg_core_dist/ffmpeg-core.js" });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [ready, setReady] = useState(false);
+const [video, setVideo] = useState();
+const [gif, setGif] = useState();
+
+const load = async () => {
+	await ffmpeg.load();
+	setReady(true);
+};
+
+useEffect(() => {
+	load();
+}, []);
+
+const convertToGif = async () => {
+	// Write the .mp4 to the FFmpeg file system
+	ffmpeg.FS("writeFile", "video1.mp4", await fetchFile(video));
+	
+	// Run the FFmpeg command-line tool, converting
+	// the .mp4 into .gif file
+	await ffmpeg.run(
+	"-i",
+	"video1.mp4",
+	"-t",
+	"2.5",
+	"-ss",
+	"2.0",
+	"-f",
+	"gif",
+	"out.gif"
+	);
+	// Read the .gif file back from the FFmpeg file system
+	const data = ffmpeg.FS("readFile", "out.gif");
+	const url = URL.createObjectURL(
+	new Blob([data.buffer], { type: "image/gif" })
+	);
+	setGif(url);
+};
+
+const download = (e) => {
+	console.log(e.target.href);
+	fetch(e.target.href, {
+	method: "GET",
+	headers: {},
+	})
+	.then((response) => {
+		response.arrayBuffer().then(function (buffer) {
+		const url = window.URL.createObjectURL(new Blob([buffer]));
+		const link = document.createElement("a");
+		link.href = url;
+		link.setAttribute("download", "image.gif");
+		document.body.appendChild(link);
+		link.click();
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+};
+
+return ready ? (
+	<div className="App">
+	<Header />
+	{video && <Inputvideo video={video} />}
+	<Inputfile setVideo={setVideo} />
+	<Button convertToGif={convertToGif} />
+	<h1>Result</h1>
+	{gif && <Resultimg gif={gif} />}
+	{gif && <Dbutton gif={gif} download={download} />}
+	</div>
+) : (
+	
+<p>Loading...</p>
+
+);
 }
 
 export default App;
---
+++ src/components/Inputvideo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Video = styled.video`
+width: 40%;
+margin: 20px;
+border: 1px dashed #045ca3;
+`;
+
+export const Inputvideo = ({ video }) => {
+return <Video controls width="250" src={URL.createObjectURL(video)} />;
+};
