67076f9899f15805282e98ebc8ea169dd3c72436
okorkov <alex.okorkov@gmail.com>
2022-02-25 11:06:58 -0800

    firefox config changes

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import Button from '@mui/material/Button';
 
 export const DownloadButton = ({ gif, download }) => {
+  const isFirefox = typeof InstallTrigger !== 'undefined';
   return (
     <div>
-      <Button variant="contained" href={gif} download onClick={(e) => download(e)}>Download Gif</Button>
+      {
+        isFirefox ?
+        <Button variant="contained" href={gif} download="processed_gif.gif" onClick={(e) => download(e)}>Download Gif</Button>
+        :
+        <Button variant="contained" href={gif} download onClick={(e) => download(e)}>Download Gif</Button>
+      }
     </div>
   );
 };
