5201e0aaa485a60ff7c2152062128c6ed52197a7
okorkov <alex.okorkov@gmail.com>
2022-03-08 09:01:12 -0800

    new config for videos, fps drop

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
       '-i',
       'video1.mp4',
       '-filter_complex',
-      '[0:v] fps=15,scale=800:-1,split [a][b];[a] palettegen=stats_mode=full [p];[b][p] paletteuse=new=1',
+      '[0:v] fps=10,scale=800:-1,split [a][b];[a] palettegen=stats_mode=full [p];[b][p] paletteuse=new=1',
       'out.gif'
     )
 
